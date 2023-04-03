a0c7f481a2938beba3f3705fd7b434130dee10a2
macnult <113482585+macnult@users.noreply.github.com>
2023-04-03 14:19:05 -0400

    Prepare step 4 of Assignment

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function getComputerChoice(){
 console.log(getComputerChoice());
 
 // * * * // 
+
+/* Step 4: 
+
+Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+*/
