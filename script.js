15e53117eea85ef904a9a77b76091399cb162004
macnult <113482585+macnult@users.noreply.github.com>
2023-04-03 07:43:18 -0400

    Complete step 3 and clean up code

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
 console.log('test');
 
-// Currently on Step 3 of Assignment
+/* "Step 3: Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’."" */
 
-/*  Begin with a function that will randomly return either 'Rock', 'Paper', or 'Scissors'. 
-
-function will be called: getComputerChoice
+function getComputerChoice(){
+    const options = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * options.length);
+    return options[random];
+}
+console.log(getComputerChoice());
 
 // * * * // 
-
-return rock
-return paper
-return scissors
-
-if i do that, it'll return the three listed in order, not pick one of three
-
-need some kind of random number generator, but turn the number range into a selection of strings
-
-maybe an array?
-
-arrayName[rock, paper, scissors] */
-
-// * * * //
-
-// defines the three options
-const options = ["Rock", "Paper", "Scissors"];
-// returns a random argument from options array
-const random = Math.floor(Math.random() * options.length);
-
-console.log(options[random]);
-// Test successful, now to make this into a function
-
-// * * * //
-
-// Create function getComputerChoice that accomplishes what you just did above
-function getComputerChoice(){
-
-}
