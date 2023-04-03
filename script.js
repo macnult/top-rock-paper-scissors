75d1897325751d4277916bc1b2328f26e691e80d
macnult <113482585+macnult@users.noreply.github.com>
2023-04-02 22:18:38 -0400

    Rough in step 3 of assignment
    also gave html body a background color of blanchedalmond

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('test');
+
+// Currently on Step 3 of Assignment
+
+/*  Begin with a function that will randomly return either 'Rock', 'Paper', or 'Scissors'. 
+
+function will be called: getComputerChoice
+
+// * * * // 
+
+return rock
+return paper
+return scissors
+
+if i do that, it'll return the three listed in order, not pick one of three
+
+need some kind of random number generator, but turn the number range into a selection of strings
+
+maybe an array?
+
+arrayName[rock, paper, scissors] */
+
+// * * * //
+
+// defines the three options
+const options = ["Rock", "Paper", "Scissors"];
+// returns a random argument from options array
+const random = Math.floor(Math.random() * options.length);
+
+console.log(options[random]);
+// Test successful, now to make this into a function
+
+// * * * //
+
+// Create function getComputerChoice that accomplishes what you just did above
+function getComputerChoice(){
+
+}
