68412d842f7b13ef6b642bd5226b3688bc6ef7a8
macnult <113482585+macnult@users.noreply.github.com>
2023-04-04 16:00:50 -0400

    Working notes for step 4-5

--- script.js
+++ script.js
@@ -11,10 +11,36 @@ console.log(getComputerChoice());
 
 // * * * // 
 
-/* Step 4: 
+/* Step 4-5: 
+Write a function that plays a single round of Rock Paper Scissors. 
 
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+The function should take two parameters - the playerSelection and computerSelection - 
+* and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+* Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). 
+
+Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:*/
+// Sample code provided by TOP 
+/*
+function playRound(playerSelection, computerSelection){
+ 
+}
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
+*/
+// * * * //
+/*
+
+working through the steps: 
+
+define playerSelection which will take the users input 
+
+input should be "rock/paper/scissors"
+
+- make it somehow restricted to those three inputs (maybe an if/else statement 
+that specifies it won't take anything other than RPS input)
+- for the correct input, make sure its case insensitive.
 
 */
+
