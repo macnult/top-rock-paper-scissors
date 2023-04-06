b11bea743d47b023be99e6c57e2cb33bcc193407
macnult <113482585+macnult@users.noreply.github.com>
2023-04-06 19:19:38 -0400

    Uh oh thunderstorm

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function playRound(playerSelection, computerSelection) {
 // * * * //
 /*
 
-Still to-do: 
+Still to-do/Step 6: 
 * Write a new function called game()
 - game() will loop playRound 5 times (or you can just call playRound x5)
 // Maybe attempt the latter before going in to the former. read up on for/while
@@ -56,12 +56,37 @@ Still to-do:
 
 * * * 
 
-playRound needs to store the score, then run again
+first things first: playRound needs to store the score, then run again
 within the rules of game() should run 5 times
 // what if it's a tie tho? play again?
-// make the for/while run to best of 5, not just 5 times
-
-
-
+// make the for/while run to best of 5, not just 5 times 
 
 */
+// * * * //
+
+// make it run more than once: call it twice?
+// add it in to game() because i think that's how it's gonna work
+
+
+// function game(){
+/*
+run playRound
+store the score
+print 
+^ we already have this written out 
+
+* may be time to write something else here that will add on to it
+* this might be the meat of turning this from hard code to a loop
+// for now, make it just play another round, adding previous score with 
+// new score and printing it out
+
+lets name it playAgain
+playAgain will run playRound, but add from the last game
+
+try calling playerScore and computerScore, but how will you get it to save when it runs the function again?
+*/
+// }
+
+function game(){
+    
+}
