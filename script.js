1fa11b3a61a3074ff186c8d240ed1125e2056d90
macnult <113482585+macnult@users.noreply.github.com>
2023-04-07 17:51:41 -0400

    Spitballing in script.js

--- script.js
+++ script.js
@@ -13,33 +13,32 @@ console.log("This tests getComputerChoice(): " + getComputerChoice());
 
 /* Step 4-5: Write a function that plays a single round of Rock Paper Scissors.  */
 
-// score variables
-let playerScore = 0;
-let computerScore = 0;
-
-// cleaner code: 
 function playRound(playerSelection, computerSelection) {
-    let message = "";
-    if (playerSelection === computerSelection) {
-      message = "It's a tie!";
-    } else if (
-      (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
-      (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
-      (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
-    ) {
-      playerScore++;
-      message = `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-      computerScore++;
-      message = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-    return message;
-  };
+  if (playerSelection == computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
+  ) {
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+};
   
-  // Might Switch to this once the game() function is made 
-  //   let playerSelection = prompt("Choose your fighter")
+ 
+
+
+  // score variables
+  let playerScore = 0;
+  let computerScore = 0;
   // temp command
   const playerSelection = "Rock";
+  // Might Switch to this once the game() function is made: 
+  // let playerSelection = prompt("Choose your fighter")
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
   console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
--- test.js
+++ test.js
@@ -1,33 +1,51 @@
-function getComputerChoice(){
-    const options = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * options.length);
-    return options[random];
-}
 
-console.log("This tests getComputerChoice(): " + getComputerChoice());
+
+function getComputerChoice(){
+  const options = ["Rock", "Paper", "Scissors"];
+  const random = Math.floor(Math.random() * options.length);
+  return options[random];
+}
 
 let playerScore = 0;
 let computerScore = 0;
+const playerSelection = "Rock";
+// Might Switch to this once the game() function is made: 
+// let playerSelection = prompt("Choose your fighter")
+const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    let message = "";
-    if (playerSelection == computerSelection) {
-      message = "It's a tie!";
-    } else if (
-      (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
-      (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
-      (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
-    ) {
-      playerScore++;
-      message = `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-      computerScore++;
-      message = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-    return message;
-  };
+  if (playerSelection == computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
+  ) {
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+};
 
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
+console.log(playRound(playerSelection, computerSelection));
+console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
+// just doubling this makes it print the same thing twice 
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
+
+function game(playerSelection, computerSelection){
+if (playerScore < 5 || computerScore < 5){
+  playRound(playerSelection, computerSelection);
+} else if (playerScore = 5){
+  console.log(`Player wins!`);
+  return;
+} else {
+  console.log(`Computer wins :(`);
+  return;
+}
+return game();
+}
+
+console.log(game(playerSelection, computerSelection));
