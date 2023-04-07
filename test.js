4b46f74aad0daa86226dc6a85b03b1da7842edba
macnult <113482585+macnult@users.noreply.github.com>
2023-04-07 18:36:22 -0400

    Reroute to script.js

--- test.js
+++ test.js
@@ -2,6 +2,36 @@
 
 Change test.js to how it should be 
 
+Write something similar to rock paper scissors but let's make it simplified:
+compare two things
 
-
+Let's try a coin flip
 */
+
+function coinFace(){
+  const faces = ["Heads", "Tails"];
+  const random = Math.floor(Math.random() * faces.length);
+  return faces[random];
+}
+
+console.log("Coin landed on: " + coinFace());
+
+// default score
+let heads = 0;
+let tails = 0; 
+
+// No need to complicate this
+const player = "Heads"
+const coin = coinFace();
+
+function flipCoin(player, coin){
+  if (player == coin){
+    console.log("It's heads!");
+    return heads++;
+  } else {
+    console.log("It's tails!");
+    return tails++;
+  }
+};
+
+console.log(flipCoin(player.heads, coin.tails));
