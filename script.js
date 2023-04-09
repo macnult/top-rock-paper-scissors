15b85c55f3615d58da19f9e0a658ed5730fbcaf6
macnult <113482585+macnult@users.noreply.github.com>
2023-04-09 12:22:26 -0400

    Save progress

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 
 /* "Step 3: Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return ‘Rock’, ‘Paper’ or ‘Scissors’."" */
-
+// This function is all set
 function getComputerChoice(){
     const options = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * options.length);
     return options[random];
 }
+// * * * //
+
+// Let's create a function now to replace the hard-coded playerSelection
+// if the prompt is not rock, paper, or scissors: needs to stop and ask user to retry input
+function getPlayerChoice(){
+    let choice = prompt("Choose your fighter! Rock, Paper, or Scissors?");
+    if (choice === null){
+      return prompt("Whoops! Try again!");
+    } else {
+      // Should be able to remove [playerSelection = playerSelection.toLowerCase();] once function getPlayerChoice() is sorted out
+      choice = choice.toLowerCase();
+      return choice;
+    }
+}
+
 // test
 console.log("This tests getComputerChoice(): " + getComputerChoice());
+
+  let playerScore = 0;
+  let computerScore = 0;
+  // Change to function getPlayerChoice when that's all said and done
+  let playerSelection = "RoCk";
+  playerSelection = playerSelection.toLowerCase();
+  let computerSelection = getComputerChoice();
 
 /* Step 4-5: Write a function that plays a single round of Rock Paper Scissors.  */
 
@@ -15,9 +37,9 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "It's a tie!";
   } else if (
-    (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
-    (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
-    (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
+    (playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
   ) {
     console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
     return playerScore++;
@@ -26,33 +48,28 @@ function playRound(playerSelection, computerSelection) {
     return computerScore++; 
   }
 };
-  
-  // score variables
-  let playerScore = 0;
-  let computerScore = 0;
- 
 
-  // Might Switch to this once the game() function is made: 
-  // let playerSelection = prompt("Choose your fighter")
+// Loops playRound until a score of 5 has been reached from either side
+function game(){
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
+  }
+}
+game();
+    
 
-  // Note: The first run of this does not have any number at the end
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
+
+
+
+
+// * * * //
+  // Old hard code prior to making game()
+
   // console.log("* * * Round 1 * * *");
   // console.log(playRound(playerSelection, computerSelection));
   // console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
 
-  function game(){
-    for (let i = 0; i < 5; i++) {
-      console.log(playRound(playerSelection, computerSelection));
-      console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`);
-    }
-  }
-game();
-
-// * * * //
-
-  // Lets try the hard code method to pull this 4 more times
   // const playerSelection2 = "Paper";
   // const computerSelection2 = getComputerChoice()
   // console.log("* * * Round 2 * * *");
@@ -76,17 +93,6 @@ game();
   // console.log("* * * Round 5 * * *");
   // console.log(playRound(playerSelection5, computerSelection5));
   // console.log(`The current score is Player: ${playerScore} Computer: ${computerScore}`)
-
-
 // * * * //
 /*
-
-Still to-do/Step 6: 
-* Write a new function called game()
-- game() will loop playRound 5 times (or you can just call playRound x5)
-// Maybe attempt the latter before going in to the former. read up on for/while
-
-* Remember to change playerSelection to a prompt() so that they can enter their own choice rather than it say "Rock" every time 
-
-*/
 
